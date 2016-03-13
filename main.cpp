2309d535487bb7555e8f5f3bdbbabd96d35957ce
janziska <jwnf7b@mail.umkc.edu>
2016-03-12 18:53:20 -0600

    Initial Build

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*********************************************
+
+Name:			Joshua Neustrom, jwnf7b@mail.umkc.edu, 10227835
+
+Course:			CS303
+Program:		Project2 (Wookie Workgroup)
+Team:			Team1
+Due Date:		March 12, 2016
+Description:	Infix Parser
+Inputs:			User given polynomial
+Outputs:		Result of polynomial adddition sorted by exponet with terms simplified
+
+*********************************************/
+
+#include <iostream>
+#include <string> 
+
+
+using namespace std;
+
+
+
+int main()
+{
+}
