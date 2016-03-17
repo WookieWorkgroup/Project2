5d0b09f2192bc7560ccf4f2beb6cbca96036ef3c
janziska <jwnf7b@mail.umkc.edu>
2016-03-17 16:25:29 -0500

    Inital integration and testing  of code examples from class

--- main.cpp
+++ main.cpp
@@ -14,7 +14,10 @@ Outputs:		Result of polynomial adddition sorted by exponet with terms simplified
 
 #include <iostream>
 #include <string> 
-
+#include <stack>
+#include "Syntax_Error.h"
+#include "Postfix_Evaluator.h"
+#include "Infix_To_Postfix.h"
 
 using namespace std;
 
@@ -22,4 +25,27 @@ using namespace std;
 
 int main()
 {
+	// Variables for user input, converted postfix expression, final result, conversions
+	string user_input = "( 3 + 2 ) * 5 - 4";
+	string post_exp = "";
+	int result = 0;
+	Infix_To_Postfix in_to_post;
+	Postfix_Evaluator post_eval;
+
+	// Show input, requires spaces to tokenize properly
+	cout << "Your input is " << user_input << endl << endl;
+	system("pause");
+
+	// Convert to a postfix expression
+ 	post_exp = in_to_post.convert(user_input);
+	cout << endl;
+	cout << "Your postfix expression is " << post_exp << endl << endl;
+	system("pause");
+
+	// Eval the postfix
+	result = post_eval.eval(post_exp);
+	cout << endl;
+	cout << "Your result is " << result << endl << endl;
+	system("pause");
+
 }
